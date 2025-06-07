f6b155d8fec5a71be0ff2823733a6f87af6987ad
mrshappy0 <ajs11300@gmail.com>
2025-06-07 15:41:22 -0600

    Attemp 2 at deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    base: "/mini-mealie-landing/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
