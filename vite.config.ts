2ce752a1ab0c7fcbcd023bc7f1fa94b7fbfe8232
mrshappy0 <ajs11300@gmail.com>
2025-06-07 16:21:05 -0600

    change route to mini-mealie

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/mini-mealie-landing/",
+    base: "/mini-mealie/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
